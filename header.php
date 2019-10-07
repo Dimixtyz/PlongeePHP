901ccbb7d92b2f563dc81a4782280b62fc0d5087
benjaminpicot <benpicot4@gmail.com>
2019-10-07 15:50:27 +0200

    b

---
+++ header.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Syllabus IAE</title>
+        <meta content="text/html; charset=UTF-8" http-equiv="content-type"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" type="text/css" href="/frontend/css/style.css">
+        <script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>
+        <script src="script.js"></script>
+    </head>
+    <body>
+
+        <nav class="navBarSyllabus">
+            <div class="nav-wrapper">
+                <a href="../index.php" class="brand-logo" style="margin-left:1%">Syllabus IAE</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="../index.php">Accueil</a></li>
+                    <li><a href="/frontend/formPersonne.htm">Formulaire personne</a></li>
+                    <li><a href="/frontend/formPlongee.htm">Formulaire plongée</a></li>
+                    <li><a href="/frontend/formPalanquee.htm">Formulaire palanquée</a></li>
+                    <li><a href="/frontend/inscriptionPersonne.htm">Inscription personne</a></li>
+                    <li><a href="/backend/deconnexion.php" onclick = "javascript:confirm('êtes-vous certain ?')">Déconnexion</a></li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
