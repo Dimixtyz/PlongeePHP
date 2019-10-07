12f6ebd67570314295f3578f5c5ae75609cf5ac5
benjaminpicot <benpicot4@gmail.com>
2019-10-07 15:34:04 +0200

    index

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Plongée</title>
+        <link rel="stylesheet" href="/frontend/css/style.css">
+        <script src="script.js"></script>
+    </head>
+
+    <body>
+        <p>Bienvenue sur le site du projet plongée</p>
+    </body>
+</html>
