16e2afda43119d2a447a63bf54e1131f5e87fd51
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-11-05 14:39:09 +0100

    css

--- frontend/recherche_personne.php
+++ frontend/recherche_personne.php
@@ -10,11 +10,9 @@ include "../header.php";
     </head>
 
     <body>
-
-    <link rel="stylesheet" type="text/css" href="css/style.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="js/customJs.js"></script>
+
     <script type="text/javascript">
 
 
