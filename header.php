21ac1015655e35842515032982fc5a472b080f29
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-11-05 17:48:00 +0100

    design

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div class="navbar-fixed">
-            <nav class="navBarPlongee" style="width:100%">
+            <nav class="blue darken-4" style="width:100%">
                 <div class="nav-wrapper">
                     <a href="../index.php" class="brand-logo left" style="padding-left:10px;">Site de Plongée</a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
